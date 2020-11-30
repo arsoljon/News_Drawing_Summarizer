a2b344cd2f154e23d09ea5ef98f1e3f3b758e43d
arsoljon <jonathan.arsola01@utrgv.edu>
2020-11-30 02:04:04 -0600

    added file to store the short stories. Added functions aligning with the methods to extract info about a short story; mentioned in the sources file.

---
+++ Code/shortstroy-understanding/shortstory-methods.py
@@ -0,0 +1,99 @@
+import requests
+from bs4 import BeautifulSoup as bs
+#get a short story from list of stories
+
+'''
+    Opening:
+    Rewrite the first paragraph  slowly.
+    Why  did  the  author  start where he did?
+    Why did he choose these words and sentences?
+'''
+#Instead of pages, think in terms of paragraphs.
+#rewrite the first paragrahp? Is there a use for this in terms of understanding the article?
+#Does this mean rewrite in terms of synonyms or conceptually? Both?
+def opening_rewrite():
+    return
+def opening_theme():
+    return
+#discover the why as to why a 'journalist' or 'author' wrote the first paragraph?
+def opening_explain():
+    return
+'''
+    Character: 
+    How does the author introduce the main character?
+    How does he or she introduce other characters? 
+    Does the character transform? How? 
+    What event caused the character to transform?
+'''
+def character_main():
+    return
+def character_explain():
+    return
+def character_events():
+    return
+
+'''
+    Mood: 
+    What is the mood of the story?
+    How is the mood established?
+'''
+def mood():
+    return
+def mood_instances():
+    return
+
+'''
+    Central event: 
+    What is the central event? 
+    Does the story have one?
+'''
+def main_event():
+    return
+
+'''
+    Structure: 
+    Create a simple outline of the story  
+    What  are  three  or  four important moments? 
+    Is there a hole in the story? 
+    Do you see bookends?
+'''
+def outline():
+    return
+def outline_moments():
+    return
+def outline_hole():
+    return
+
+'''
+    Ending:
+    How   does   the   author   end   the   story?   
+    Read   the   last   page painstakingly slowly. 
+    Why did the author end there? 
+    Why did he or she use those words and sentences?
+'''
+def ending():
+    return
+def ending_theme():
+    return
+
+'''
+    Narrative Mode: 
+    What tense (past, present) is the story in?   
+    What point of view  does  the  author  use  
+        (first-person,  third-person  omniscient,  third-person  limited,  second-person)? 
+    How  does  that  affect  the  way  the  story  is told? 
+'''
+
+'''
+    Voice:
+    Which elements of a story does the author rely on 
+        (see chapter three of Let's Write a Short Story)? 
+    Skim a few other stories from that magazine. 
+    Do they have a similar tone?
+'''
+
+'''
+    Transitions: 
+    How does the author transition from one scene to the next?
+'''
+print("Hello world")
